6c2162173166834b4411b50bd8b84a1f00870e66
weedii <abidiw293@gmail.com>
2025-01-12 22:40:34 +0100

    adding chat history

--- server/app/utils/generateResponse_v2.py
+++ server/app/utils/generateResponse_v2.py
@@ -2,6 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 import os
+from langchain.schema import HumanMessage, AIMessage
 
 load_dotenv()
 openApiKey = os.getenv("OPENAI_API_KEY")
@@ -17,6 +18,8 @@ Question: {question}
 Answer the question thoroughly but concisely and retun only response text:
 """
 
+chatHistory = []
+
 
 def generateFormattedPrompt(data: str, prompt: str):
     promptTemplate = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
@@ -27,7 +30,11 @@ def generateFormattedPrompt(data: str, prompt: str):
 def generateResponseFromPrompt(data: str, prompt: str):
     formattedPrompt = generateFormattedPrompt(data, prompt)
 
+    chatHistory.append(HumanMessage(content=formattedPrompt, role="user"))
+
     model = ChatOpenAI(openai_api_key=openApiKey)
-    res = model.invoke(formattedPrompt)
+    res = model.invoke(chatHistory)
+
+    chatHistory.append(AIMessage(content=res.content, role="assistant"))
 
     return res.content
