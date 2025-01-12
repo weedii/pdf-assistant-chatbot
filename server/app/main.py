2e074bf0476a60d81181652b2071d2ba54d3d628
weedii <abidiw293@gmail.com>
2025-01-12 12:43:20 +0100

    cors

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from app.routers import entryRouter, pdfRouter, chatRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origings = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origings,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(entryRouter)
 app.include_router(pdfRouter)
